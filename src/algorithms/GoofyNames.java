9e4b6a21e5a633aa50582c011334d54eb815d0f3
347356 <347356@0338S-S-R0B6ZGX.sdcs.local>
2018-10-02 14:25:44 -0700

    Code4Life - EvilPolygonsChallenge - GoofyNames - GuessingPi

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -11,26 +11,30 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+		String Username = JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+		System.out.println(Username.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
-
+		for (int i=0;i<Username.length();i++) {
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
+			String Character = Username.substring(i, i+1);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-
+			if (i%2 == 0) {
+				Character = Character.toUpperCase();
+			} else {
+				Character = Character.toLowerCase();
+			}
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
-
-		
+			goofyName = goofyName+Character;
+		}
 		// 7. Use pop-up to show user their Goofy name
-
+		JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
 
--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -10,17 +10,51 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-
+		Robot robot = new Robot();
+		
+		// Default Settings //
+		int WinX = 1000;
+    	int WinY = 1000;		
+		
+    	robot.penDown();
+    	robot.setSpeed(100);
+    	robot.setWindowSize(WinX, WinY);
+    	robot.moveTo(WinX/2, WinY/2);
+    	robot.setRandomPenColor();
+    	robot.setPenWidth(1);
+    	robot.miniaturize();
+    	robot.setX(50);
+    	robot.setY(WinY/2);
+    	
 		//2. Set the speed to 100
 
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+		int colorChoice=JOptionPane.showOptionDialog(null, "Choose a color", "Evil Polygons", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Black", "Random"}, 0);
+		System.out.println(colorChoice);
 		
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
+		if (colorChoice == 0) {
+			robot.setPenColor(Color.BLACK);
+		}
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		int ShapesNum = Integer.parseInt(JOptionPane.showInputDialog("How many polygons do you want to be drawn?"));
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		for (int i=0;i<ShapesNum;i++) {
+			int Sides = 6;
+			int Width = 100;
+			
+			robot.penDown();
+			robot.setAngle(90);
+			
+			for (int t=0;t<Sides;t++) {
+				robot.move(Width/Sides);
+				robot.turn(360/Sides);
+			}
+			
+			robot.penUp();
+			
+			robot.setAngle(90);
+			robot.move(Width/2);
+		}
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
